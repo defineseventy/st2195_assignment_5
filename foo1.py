e8a806fd7dd5138d58da250aa3d749d578bbb3f2
Jacqueline <jacweizhen6679@gmail.com>
2024-12-18 12:19:52 +0800

    foo1

--- foo1.py
+++ foo1.py
@@ -8,14 +8,16 @@ def is_divisible_by_k(i, k):
 Store all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding 
 doubles) 
 ''' 
-x = [] 
-for i in range(1001): 
-    if is_divisible_by_k(i, 2) or is_divisible_by_k(i, 3) or is_divisible_by_k(i, 7): 
-        x.append(i)
+x = set()
+#x = [] 
+for i in range(1, 1001): 
+    if is_divisible_by_k(i, 2) or is_divisible_by_k(i, 5) or is_divisible_by_k(i, 7): 
+        #x.append(i) have to use list if don't wanna change append
+        x.add(i)
      
 ''' 
 Sum all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding 
 doubles) 
 ''' 
-x = sum(x)
-print(x)
+#x = sum(x)
+print(sum(x))
